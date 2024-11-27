0584296c2533fbeb6eafe119572d17c4be58eb4a
Ayan Williams <101142116+Ionwill@users.noreply.github.com>
2024-11-27 16:05:57 -0600

    push

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle
+        
+        
+        func softEggTime(){
+            print(softTime)
+            
+        }
+        
+        func mediumEggTime(){
+            print(mediumTime)
+                    
+        }
+        
+        func hardEggTime(){
+            print(hardTime)
+            
+        }
+        
+        
+    }
 }
